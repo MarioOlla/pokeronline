5cecaf571ccbaa4c2154757a75ea374e39159c28
MarioOlla <mario.olla25@gmail.com>
2022-11-29 12:01:01 +0100

    create le classi di model

---
+++ src/main/java/it/prova/pokeronline/model/Tavolo.java
@@ -0,0 +1,114 @@
+package it.prova.pokeronline.model;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tavolo")
+public class Tavolo {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id ;
+	@Column(name = "esperienzaMin")
+	private Integer esperienzaMin;
+	@Column(name = "cifraMinima")
+	private Integer cifraMinima;
+	@Column(name = "denominazione")
+	private String denominazione;
+	@Column(name = "dataCreazione")
+	private LocalDate dataCreazione;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "utente_id", nullable = false)
+	private Utente utenteCreazione;
+	@ManyToMany
+	@JoinTable(name = "tavolo_utente",joinColumns = @JoinColumn(name = "tavolo_id", referencedColumnName = "ID"), inverseJoinColumns = @JoinColumn(name = "utente_id", referencedColumnName = "ID"))
+	private Set<Utente> giocatori = new HashSet<>();
+	
+	public Tavolo() {
+		
+	}
+
+	public Tavolo(Long id, Integer esperienzaMin, Integer cifraMinima, String denominazione, LocalDate dataCreazione,
+			Utente utenteCreazione, Set<Utente> giocatori) {
+		super();
+		this.id = id;
+		this.esperienzaMin = esperienzaMin;
+		this.cifraMinima = cifraMinima;
+		this.denominazione = denominazione;
+		this.dataCreazione = dataCreazione;
+		this.utenteCreazione = utenteCreazione;
+		this.giocatori = giocatori;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getEsperienzaMin() {
+		return esperienzaMin;
+	}
+
+	public void setEsperienzaMin(Integer esperienzaMin) {
+		this.esperienzaMin = esperienzaMin;
+	}
+
+	public Integer getCifraMinima() {
+		return cifraMinima;
+	}
+
+	public void setCifraMinima(Integer cifraMinima) {
+		this.cifraMinima = cifraMinima;
+	}
+
+	public String getDenominazione() {
+		return denominazione;
+	}
+
+	public void setDenominazione(String denominazione) {
+		this.denominazione = denominazione;
+	}
+
+	public LocalDate getDataCreazione() {
+		return dataCreazione;
+	}
+
+	public void setDataCreazione(LocalDate dataCreazione) {
+		this.dataCreazione = dataCreazione;
+	}
+
+	public Utente getUtenteCreazione() {
+		return utenteCreazione;
+	}
+
+	public void setUtenteCreazione(Utente utenteCreazione) {
+		this.utenteCreazione = utenteCreazione;
+	}
+
+	public Set<Utente> getGiocatori() {
+		return giocatori;
+	}
+
+	public void setGiocatori(Set<Utente> giocatori) {
+		this.giocatori = giocatori;
+	}
+		
+}
